9212a07ad2616f15e1f3caf52f6aa061f3397470
rhernandez1996 <rayvenhernandez14@yahoo.com>
2022-10-02 20:02:21 -0700

    adding table data1

---
+++ code.js
@@ -0,0 +1,30 @@
+{
+    $( ".title" ).append( "<caption>Calories Comparison of Popular Burgers</caption>" );
+    $( ".heading" ).append( "<th>Type</th>" );
+    $( ".head1" ).append( "<th>BigMac</th>" );
+    $( ".head2" ).append( "<th>Whopper</th>" );
+    $( ".head3" ).append( "<th>Dave's Double</th>" );
+    $( ".head4" ).append( "<th>Whataburger</th>" );
+}
+{
+    $( ".subheadcal" ).append( "<th>Calories</th>" );
+    $( ".cal1" ).append( "<th>563</th>" );
+    $( ".cal2" ).append( "<th>677</th>" );
+    $( ".cal3" ).append( "<th>870</th>" );
+    $( ".cal4" ).append( "<th>590</th>" );
+}
+{   
+     $( ".subheadfat" ).append( "<th>Fat</th>" );
+    $( ".fat1" ).append( "<th>30gm</th>" );
+    $( ".fat2" ).append( "<th>40gm</th>" );
+    $( ".fat3" ).append( "<th>51gm</th>" );
+    $( ".fat4" ).append( "<th>25gm</th>" );
+}
+{
+    $( ".subheadven" ).append( "<th>Vendor</th>" );
+    $( ".ven1" ).append( "<th>MacD</th>" );
+    $( ".ven2" ).append( "<th>Burger King</th>" );
+    $( ".ven3" ).append( "<th>Wendys</th>" );
+    $( ".ven4" ).append( "<th>Whataburger</th>" ); 
+   
+ }
